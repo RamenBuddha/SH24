a19624a9b7fcd57512918e6daae098ee8cb1613f
Arthur1asdf <arthurteng0819@gmail.com>
2024-09-28 16:49:46 -0400

    changed app.jsx so we have a clear split between keybind and the buttons and cotrol

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Navbar from './components/Navbar'
-import './App.css'
 
+import Navbar from './components/Navbar'
+
+import Joystick from './components/Joystick'
 function App() {
   return (
     <>
-    <Navbar></Navbar>
-    <div className='absolute top-0, left-0 w-full mt-6 border-2 border-black h-[40rem] flex justify-evenly space-x-4 items-center'>
-      <div className="flex-col">
-      <p>placeholder</p>
-      <p>placeholder</p>
+      <Navbar></Navbar>
+
+      <div className=" w-screen h-[50vh] bg-yellow-500 ">
+
       </div>
-      <div className="flex-col">
-      <p>placeholder</p>
-      <p>placeholder</p>
+
+      <div className="w-screen h-[50vh] bg-blue-500">
+
       </div>
-    </div>
     </>
   )
 }
