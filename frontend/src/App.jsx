1db06025f0dcd0edabfed51ba45206654076bf4c
eren <shaoyuan00145@gmail.com>
2024-09-28 19:08:49 -0400

    Added export for the buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { useState } from 'react'
 
 function App() {
   let keys = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12","`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "\\", "|", ";", ":", "'", "\"", ",", ".", "/", "<", ">", "?"];
+
   const chunkArray = (array, chunkSize) => {
     const chunks = [];
     for (let i = 0; i < array.length; i += chunkSize) {
@@ -14,21 +15,56 @@ function App() {
     }
     return chunks;
   };
+
   const keyChunks = chunkArray(keys, 4);
   const [message, setMessage] = useState('');
   const [listeningButton, setListeningButton] = useState(null); 
 
+  const [buttons, setButtons] = useState([
+    { id:1, bind: "N/A"},
+    { id: 2, bind: "N/A"}
+  ])
+
+  const createButtonsJson = () => {
+    const jsonData = JSON.stringify(buttons, null, 2); 
+    const blob = new Blob([jsonData], { type: "application/json" }); 
+    const url = URL.createObjectURL(blob); 
+
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "buttons_data.json"; 
+    a.click(); 
+    URL.revokeObjectURL(url);
+  };
+  
+  const updateButtonBind = (id, newBind) => {
+    setButtons((prevButtons) =>
+      prevButtons.map((button) =>
+        button.id === id ? { ...button, bind: newBind } : button
+      )
+    );
+  };
+
   return (
     <>
-      <MessageContext.Provider value={{ message, setMessage }}>
+      <MessageContext.Provider value={{createButtonsJson, message, setMessage }}>
       <Navbar></Navbar>
       <div className=" w-screen h-[50vh] bg-white flex justify-evenly items-center">
         <div className='flex'>
           <Joystick></Joystick>
         </div>
         <div className='flex-col space-y-4'>
-        <Button id={1} isListening={listeningButton === 1} setListeningButton={setListeningButton}></Button>
-        <Button id={2} isListening={listeningButton === 2} setListeningButton={setListeningButton}></Button>
+        {buttons.map((button)=>(
+          <Button
+          key={button.id}
+          id={button.id} 
+          bind={button.bind} 
+          isListening={listeningButton === button.id} 
+          setListeningButton={setListeningButton}
+          updateButtonBind={updateButtonBind} 
+        />
+        ))}
         </div>
       </div>
       
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,18 @@
+import MessageContext from "./MessageContext";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    const {createButtonsJson} = useContext(MessageContext);
+
+    const handleClick = () => {
+        createButtonsJson();
+    }
     return (
         <>
         <header class="bg-black px-16 py-2 z-10 w-full fixed flex items-center justify-between left-0 top-0 box-border">
         <span className="hover:cursor-pointer hover:transition duration-350 ease-in-out hover:text-slate-400 font-bold text-white text-left">SH24</span>
-        <span className="transition-transform hover:scale-105 rounded-sm px-6 py-2 bg-white hover:cursor-pointer hover:transition duration-350 ease-in-out font-bold text-black">Export</span>
+        <button onClick ={handleClick} className="transition-transform hover:scale-105 rounded-sm px-6 py-2 bg-white hover:cursor-pointer hover:transition duration-350 ease-in-out font-bold text-black">Export</button>
         </header>        
         </>
     )
