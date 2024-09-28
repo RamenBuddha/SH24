5a55c45607d288562e3ba59e9dba3775e6c8ba95
eren <shaoyuan00145@gmail.com>
2024-09-28 16:45:32 -0400

    Moved components into component folder and created fleshed out buttons with some in the placeholder

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect} from "react"
+
+const Button = (key) => {
+    const[bind,setBind] = useState("");
+    const [isListening, setIsListening] = useState(false);
+
+    const handleClick = () => {
+        setIsListening(true);
+    }
+
+    const handleKeyPress = (event) => {
+        if (isListening) {
+            if (event.key === " "){
+                setBind("Space".toUpperCase());
+            } else{
+                setBind(event.key.toUpperCase());
+            }
+        }
+        setIsListening(false);
+    }
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyPress);
+        return () => {
+            window.removeEventListener("keydown", handleKeyPress);
+          };
+    }, [isListening])
+
+    return (
+        <>
+        <button onClick={handleClick} className="rounded-full border-2 p-2 w-16 h-16 border-black flex items-center justify-center">
+        {bind}
+        </button>        </>
+    )
+}
+export default Button
