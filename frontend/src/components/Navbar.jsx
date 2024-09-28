9b02c79574ef352745aad016b2784bfa6e4d0a8c
eren <shaoyuan00145@gmail.com>
2024-09-28 02:49:10 -0400

    made a test page and added frontend instructions

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <>
+        <p className="bg-blue-50 text-bold">
+            Test
+        </p>
+        </>
+    )
+}
+
+
+export default Navbar
