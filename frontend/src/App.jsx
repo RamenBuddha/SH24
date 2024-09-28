c7c8504b89b8412b0275c025626e7ddef21e348a
eren <shaoyuan00145@gmail.com>
2024-09-28 17:17:32 -0400

    Testing keys

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-
 import Navbar from './components/Navbar'
 import Button from './components/Button'
-
 import Joystick from './components/Joystick'
+import Key from './components/Key'
+
 function App() {
   return (
     <>
@@ -18,8 +18,22 @@ function App() {
         </div>
       </div>
 
-      <div className="w-screen h-[50vh] bg-slate-100">
-
+      <div className="items-center grid grid-cols-6 justify-center w-screen h-[50vh] bg-slate-100 space-x">
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        <Key></Key>
+        
       </div>
     </>
   )
---
+++ frontend/src/components/Key.jsx
@@ -0,0 +1,9 @@
+const Key = (key) =>{
+    return (
+        <>
+        <button className="rounded-full w-16 h-16 border-2 border-black"></button>
+        </>
+    )
+}
+
+export default Key
