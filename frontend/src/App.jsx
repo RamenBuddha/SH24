c77e2ecfd10dda4cbf3dbd9f460850ba08e29e76
eren <shaoyuan00145@gmail.com>
2024-09-28 03:45:20 -0400

    added placeholder for where the buttons go

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,19 @@ import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <Navbar></Navbar>
-     
+    <div className='absolute top-0, left-0 w-full mt-6 border-2 border-black h-[40rem] flex justify-evenly space-x-4 items-center'>
+      <div className="flex-col">
+      <p>placeholder</p>
+      <p>placeholder</p>
+      </div>
+      <div className="flex-col">
+      <p>placeholder</p>
+      <p>placeholder</p>
+      </div>
+    </div>
     </>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <>
-        <p className="bg-blue-50 text-bold">
-            Test
-        </p>
+        <header class="bg-pink-300 px-16 py-2 z-10 w-full fixed flex items-center justify-between left-0 top-0 box-border">
+        <span className="hover:cursor-pointer hover:transition duration-350 ease-in-out hover:text-black font-bold text-white text-left">SH24</span>
+        <span className="rounded-sm px-6 py-2 bg-pink-200 hover:cursor-pointer hover:transition duration-350 ease-in-out hover:text-black font-bold text-white text-left">Export</span>
+        </header>        
         </>
     )
 }
