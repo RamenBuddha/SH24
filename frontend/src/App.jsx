c200256e540a20ec80eb3a5af3ebbf99b83d4209
eren <shaoyuan00145@gmail.com>
2024-09-29 01:03:42 -0400

    changed key binds

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,130 @@ import MessageContext from './components/MessageContext'
 import { useState } from 'react'
 
 function App() {
-  let keys = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "\\", "|", ";", ":", "'", "\"", ",", ".", "/", "<", ">", "?"];
-
+  let keys = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
+    "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",
+    "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "\\", "|", ";", ":", "'", "\"", ",", ".", "/", "<", ">", "?",
+    "Enter", "BKSP", "Tab", "Esc", "Space", 
+    "Up", "Down", "Left", "Right",
+    "Ctrl", "Alt", "Shift", "Win", 
+    "Caps",
+  ];
+  
+  let asciiVals = {
+    "A": 65,
+    "B": 66,
+    "C": 67,
+    "D": 68,
+    "E": 69,
+    "F": 70,
+    "G": 71,
+    "H": 72,
+    "I": 73,
+    "J": 74,
+    "K": 75,
+    "L": 76,
+    "M": 77,
+    "N": 78,
+    "O": 79,
+    "P": 80,
+    "Q": 81,
+    "R": 82,
+    "S": 83,
+    "T": 84,
+    "U": 85,
+    "V": 86,
+    "W": 87,
+    "X": 88,
+    "Y": 89,
+    "Z": 90,
+    "0": 48,
+    "1": 49,
+    "2": 50,
+    "3": 51,
+    "4": 52,
+    "5": 53,
+    "6": 54,
+    "7": 55,
+    "8": 56,
+    "9": 57,
+    "`": 96,
+    "~": 126,
+    "!": 33,
+    "@": 64,
+    "#": 35,
+    "$": 36,
+    "%": 37,
+    "^": 94,
+    "&": 38,
+    "*": 42,
+    "(": 40,
+    ")": 41,
+    "-": 45,
+    "_": 95,
+    "=": 61,
+    "+": 43,
+    "[": 91,
+    "]": 93,
+    "{": 123,
+    "}": 125,
+    "\\": 92,
+    "|": 124,
+    ";": 59,
+    ":": 58,
+    "'": 39,
+    "\"": 34,
+    ",": 44,
+    ".": 46,
+    "/": 47,
+    "<": 60,
+    ">": 62,
+    "?": 63,
+    "Enter": 13,
+    "Return": 13,
+    "BKSP": 8,
+    "Tab": 9,
+    "Esc": 27,
+    "Space": 32,
+    "Up": 218,
+    "Down": 217,
+    "Left": 216,
+    "Right": 215,
+    "Home": 36,
+    "End": 35,
+    "PageUp": 33,
+    "PageDown": 34,
+    "Ctrl": 17,
+    "Alt": 18,
+    "Shift": 16,
+    "Win": 131, // Windows key or Command key
+    "Caps": 20,
+    "NumLock": 144,
+    "ScrollLock": 145,
+    "PrintScreen": 44,
+    "Insert": 45,
+    "Delete": 46,
+    "Pause": 19,
+    "F1": 112,
+    "F2": 113,
+    "F3": 114,
+    "F4": 115,
+    "F5": 116,
+    "F6": 117,
+    "F7": 118,
+    "F8": 119,
+    "F9": 120,
+    "F10": 121,
+    "F11": 122,
+    "F12": 123
+  };
+  
+  const findVal = (obj, value) => {
+    const entry = Object.entries(obj).find(([key, val]) => val === value);
+    return entry ? entry[0] : null;
+  }
+ 
   const chunkArray = (array, chunkSize) => {
     const chunks = [];
     for (let i = 0; i < array.length; i += chunkSize) {
@@ -68,7 +190,7 @@ function App() {
   const updateButtonBind = (id, newBind) => {
     setButtons((prevButtons) =>
       prevButtons.map((button) =>
-        button.id === id ? { ...button, bind: newBind.charCodeAt(0) } : button 
+        button.id === id ? { ...button, bind: asciiVals[newBind] } : button 
       )
     );
   };
@@ -86,7 +208,7 @@ function App() {
               <Button
                 key={button.id}
                 id={button.id}
-                bind={String.fromCharCode(button.bind)}
+                bind={findVal(asciiVals, button.bind)}
                 isListening={listeningButton === button.id}
                 setListeningButton={setListeningButton}
                 updateButtonBind={updateButtonBind}
